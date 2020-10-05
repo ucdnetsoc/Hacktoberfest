e7e47a0c45b1228cd7b9aff9095ac7a7bb718d5a
Vishiel <outar>
2020-10-05 01:18:30 +0000

    Hello from the USA

---
+++ VO/hello.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+	System.out.println("こんにちは");
+	System.out.println("नमस्ते");
+	System.out.println("Hola");
+	System.out.println("你好");
+    }
+}
