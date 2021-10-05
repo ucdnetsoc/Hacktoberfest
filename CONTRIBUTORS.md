058261b322e7b57f9076eee3367e5613a94766e3
IvorLucas <ilucas.no.1@gmail.com>
2021-10-05 19:27:37 +0100

    Did something

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -1,3 +1,4 @@
 # Contributors
 
 - [Ashraf Ali](https://github.com/ash-xyz)
+- [Ivor Lucas](https://github.com/IvorLucas)
---
+++ helloworld/helloworld_ivor.c
@@ -0,0 +1,6 @@
+/* Question1.c: Hello World */
+#include <stdio.h>
+int main(void){
+  printf("hello world \n");
+  return 0;
+}
